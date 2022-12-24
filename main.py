e0da7e280e0c42a8c7dd40c2cd450ffd73b07b2f
Dor Grinshpan <dorgrin6@gmail.com>
2022-12-24 11:46:39 +0200

    add .env, some comments

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+import os
 from time import sleep
 
-from telegram.ext import Updater, CommandHandler
+import dotenv
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
 import requests
 
-WEBSITE = 'https://example.com'
+"""
+IsItUp.
+    Bot that checks polls until a website is up and let's you know when it does!
+"""
+
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 '
                   'Safari/537.36',
@@ -11,20 +18,31 @@ headers = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "Accept-Language": "en-US,en;q=0.5",
     "Accept-Encoding": "gzip, deflate"}
 
-TELEGRAM_TOKEN = '<YOUR-TOKEN>'  # follow https://core.telegram.org/bots/tutorial
-
 
 def start(update, context) -> None:
+    """
+    Callback that iterates the bot's options.
+    :param update: represents an incoming update.
+    :param context: context object passed to the callback
+    :return: None
+    """
     context.bot.send_message(chat_id=update.effective_chat.id,
                              text=f"Hello world! Type /track <url> to start tracking a url")
 
 
-def track(update, context):
-    check_webpage(update, context)
+def check_webpage(update: Update, context: CallbackContext) -> None:
+    """
+    Callback that polls until a website is up (returns 200).
+    :param update: represents an incoming update.
+    :param context: context object passed to the callback
+    :return: None
+    """
+    if len(context.args) != 1:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text=f'Please supply website to track\n/track <url>')
+        return
 
-
-def check_webpage(update, context):
-    website = context.args[0] if context.args else WEBSITE
+    website = context.args[0]
     context.bot.send_message(chat_id=update.effective_chat.id, text=f'Starting to poll for website {website}')
 
     try:
@@ -46,10 +64,15 @@ def check_webpage(update, context):
 
 
 def main() -> None:
-    updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
+    dotenv.load_dotenv()
+    telegram_token = os.getenv('TELEGRAM_TOKEN')  # to get token, follow https://core.telegram.org/bots/tutorial
+    if not telegram_token or telegram_token == "<YOUR_TELEGRAM_TOKEN>":
+        raise ValueError('Please define\nTELEGRAM_TOKEN=<YOUR_TELEGRAM_TOKEN>\nvariable in .env file')
+
+    updater = Updater(token=telegram_token, use_context=True)
 
     start_handler = CommandHandler('start', start)
-    track_handler = CommandHandler('track', track, pass_args=True)
+    track_handler = CommandHandler('track', check_webpage, pass_args=True)
 
     updater.dispatcher.add_handler(start_handler)
     updater.dispatcher.add_handler(track_handler)
